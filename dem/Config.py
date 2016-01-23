10427e4e3475fe6cf3f18f594b2b761fce5797c2
Jeremy Opalach <jeremy.opalach@gmail.com>
2016-01-22 23:06:50 -0700

    Moved classes into DevEnvReader module

--- dem/Config.py
+++
@@ -1,6 +0,0 @@
-class Config():
-    def __init__(self, dictionary={}):
-        self._config = dictionary
-
-    def __getitem__(self, item):
-        return self._config[item]
--- dem/DevEnvReader.py
+++ dem/DevEnvReader.py
@@ -1,7 +1,29 @@
 import yaml
 
-from .Packages import Packages
-from .Config import Config
+
+class Config:
+    def __init__(self, dictionary={}):
+        self._config = dictionary
+
+    def __getitem__(self, item):
+        return self._config[item]
+
+
+class Packages:
+    def __init__(self, dictionary={}):
+        self._packages = dictionary
+
+    def __getitem__(self, item):
+        return self._packages[item]
+
+    def has_a_library(self):
+        for p in self._packages.values():
+            if p['type'] == 'archive':
+                return True
+        return False
+
+    def values(self):
+        return self._packages.values()
 
 
 def _auto_populate_missing_fields(packages):
@@ -35,4 +57,4 @@ def devenv_from_file(devenv_file_path):
         config = devenv['config']
     _fixup_remote_locations(config)
 
-    return (Config(config), Packages(packages))
+    return Config(config), Packages(packages)
