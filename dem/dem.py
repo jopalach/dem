98b3acba1da93fffc81a48fd75f8a62ee55dc189
Ian Macaulay <ismacaul@gmail.com>
2016-05-07 11:49:34 -0600

    Issue #9 - Implemented hashing the yaml file so it will only update when the yaml file changes

--- dem/dem.py
+++ dem/dem.py
@@ -4,10 +4,15 @@ from . ArchiveInstaller import ArchiveInstaller
 from . import DevEnvReader as reader
 from . EnvironmentBuilder import EnvironmentBuilder
 from . RpmInstaller import RpmInstaller
-
+from . PackageCache import PackageCache
 
 def get_dem_packages(project):
     (config, packages) = reader.devenv_from_file('devenv.yaml')
+
+    cache = PackageCache(os.getcwd())
+    if not cache.needs_update():
+        print('[dem] Up to date.')
+        return
 
     EnvironmentBuilder.build(project)
 
@@ -16,6 +21,8 @@ def get_dem_packages(project):
 
     rpm_installer = RpmInstaller(packages.rpm_packages())
     rpm_installer.install_packages()
+
+    cache.update()
 
 if __name__ == '__main__':
     project = os.path.basename(os.getcwd())
---
+++ test/TestPackageCaching.py
@@ -0,0 +1,89 @@
+import unittest, mock
+import pyfakefs.fake_filesystem_unittest as fake_filesystem_unittest
+import hashlib
+import json
+import os
+
+from dem.PackageCache import PackageCache
+
+SAMPLE_YAML_CONTENT = '''
+config:
+    remote_locations:
+        ['/opt',
+        'http://github.com']
+packages:
+    qt:
+        version: 4.8.6
+        type: rpm
+    json:
+        version: 1.8
+        type: archive
+'''
+
+DIFFERENT_SAMPLE_YAML_CONTENT = '''
+config:
+    remote_locations:
+        ['/opt',
+        'http://github.com']
+packages:
+    qt:
+        version: 4.8.6
+        type: rpm
+    json:
+        version: 1.9
+        type: archive
+'''
+
+
+SAMPLE_CACHE_CONTENT = '''
+{
+    "md5": "b084deb42d4daefe7c29d4eae24c2d2d"
+}
+'''
+
+class MyTestCase(fake_filesystem_unittest.TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def test_will_hash_yaml_file(self):
+        self.fs.CreateDirectory(os.path.abspath('base_path'))
+        self.fs.CreateDirectory(os.path.join('base_path', '.devenv'))
+        self.fs.CreateFile(os.path.join('base_path', 'devenv.yaml'), contents=SAMPLE_YAML_CONTENT)
+
+        cache = PackageCache(os.path.abspath('base_path'))
+        cache.update()
+
+        hasher = hashlib.md5()
+        hasher.update(SAMPLE_YAML_CONTENT.encode('utf-8'))
+        expected_hash = hasher.hexdigest()
+        print(expected_hash)
+        with open(os.path.join('base_path', '.devenv', 'cache.json')) as f:
+            data = json.load(f)
+            self.assertEqual(data.get('md5', ''), expected_hash)
+
+    def test_needs_update_when_cache_file_does_not_exist(self):
+        self.fs.CreateDirectory(os.path.abspath('base_path'))
+        cache = PackageCache(os.path.abspath('base_path'))
+        self.assertTrue(cache.needs_update())
+
+    def test_needs_update_when_md5_hash_mismatch(self):
+        self.fs.CreateDirectory(os.path.abspath('base_path'))
+        self.fs.CreateDirectory(os.path.join('base_path', '.devenv'))
+        self.fs.CreateFile(os.path.join('base_path', 'devenv.yaml'), contents=DIFFERENT_SAMPLE_YAML_CONTENT)
+        self.fs.CreateFile(os.path.join('base_path', '.devenv', 'cache.json'), contents=SAMPLE_CACHE_CONTENT)
+
+        cache = PackageCache(os.path.abspath('base_path'))
+        self.assertTrue(cache.needs_update())
+
+    def test_does_not_need_update_when_md5_hash_match(self):
+        self.fs.CreateDirectory(os.path.abspath('base_path'))
+        self.fs.CreateDirectory(os.path.join('base_path', '.devenv'))
+        self.fs.CreateFile(os.path.join('base_path', 'devenv.yaml'), contents=SAMPLE_YAML_CONTENT)
+        self.fs.CreateFile(os.path.join('base_path', '.devenv', 'cache.json'), contents=SAMPLE_CACHE_CONTENT)
+
+        cache = PackageCache(os.path.abspath('base_path'))
+        self.assertFalse(cache.needs_update())
+
+
+if __name__ == '__main__':
+    unittest.main()
